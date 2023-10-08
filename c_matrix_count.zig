2e088bf23a02c1869d5e741bf03594d25d06b314
mavro <mrojasb2000@gmail.com>
2023-10-08 15:00:52 -0300

    chore: add operation substract data from matrix type C

---
+++ c_matrix_count.zig
@@ -0,0 +1,8 @@
+const std = @import("std");
+
+pub fn main() void {
+    const nums = [_]u8{ 2, 5, 6, 4 };
+    var x: usize = 3;
+    const count = nums[1..x];
+    std.debug.print("{any}\n", .{count});
+}
