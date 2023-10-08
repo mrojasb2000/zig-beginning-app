0a57b84d17b4a0c844c5b74f11b2f3cd0fd49326
mavro <mrojasb2000@gmail.com>
2023-10-08 14:01:25 -0300

    chore: add change ordinal numbers

--- enums.zig
+++ enums.zig
@@ -1,10 +1,10 @@
 const std = @import("std");
 
 pub fn main() void {
-    const LogType = enum {
-        infomation,
-        err,
-        warning,
+    const LogType = enum(i32) {
+        infomation = 200,
+        err = 500,
+        warning = 600,
     };
     const lfInfo = LogType.infomation;
     const lfErr = LogType.err;
