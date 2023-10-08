1a48aa3b23d06dce859109eb6627c5422c310ca4
mavro <mrojasb2000@gmail.com>
2023-10-08 19:07:28 -0300

    chore: add more pointers

--- pointers.zig
+++ pointers.zig
@@ -8,4 +8,17 @@ pub fn main() !void {
     std.debug.print("{d}\n", .{x});
     std.debug.print("{d}\n", .{ptr_x});
     std.debug.print("{}\n", .{@TypeOf(ptr_x)});
+
+    var A = [_]u8{ 2, 5, 6, 1, 1 };
+    var ptr_z = &A[1];
+    ptr_z.* = 12;
+
+    std.debug.print("{d}\n", .{A[1]});
+    std.debug.print("{d}\n", .{ptr_z});
+    std.debug.print("{}\n", .{@TypeOf(ptr_z)});
+
+    var ptr_y = &A;
+    ptr_y[2] = 11;
+    std.debug.print("{any}\n", .{A});
+    std.debug.print("{}\n", .{@TypeOf(ptr_y)});
 }
