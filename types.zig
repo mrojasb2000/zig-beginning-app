7212ccb50528479829bde7a62c643401c1ae44bc
mavro <mrojasb2000@gmail.com>
2023-10-08 13:48:55 -0300

    chore: add basic types

--- types.zig
+++ types.zig
@@ -4,8 +4,10 @@ pub fn main() void {
     var x: i8 = -100;
     var y: u8 = 120;
     var z: f32 = 100.324;
+    var initialized: bool = true;
 
     std.debug.print("x = {}\n", .{x});
     std.debug.print("y = {}\n", .{y});
     std.debug.print("z = {}\n", .{z});
+    std.debug.print("initialized = {}\n", .{initialized});
 }
