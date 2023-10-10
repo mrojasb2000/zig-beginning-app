4c9a22267e8cd9b819f0eb49a5c341688f4a28b9
mavro <mrojasb2000@gmail.com>
2023-10-10 15:05:58 -0300

    chore: add inference of type on function

--- basic/poweruser/models/user.zig
+++ basic/poweruser/models/user.zig
@@ -9,7 +9,7 @@ pub const User = struct {
     pub const SUPER_POWER = 9_000;
 
     pub fn init(name: []const u8, power: u64) User {
-        return User{
+        return .{
             .name = name,
             .power = power,
         };
