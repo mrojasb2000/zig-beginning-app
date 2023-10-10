4de0c9808c02c3e24e2e519bb87b6f6710675ca9
mavro <mrojasb2000@gmail.com>
2023-10-10 17:00:59 -0300

    chore: add array and slice upper bound runtime/comptime

--- basic/arrays/main.zig
+++ basic/arrays/main.zig
@@ -4,6 +4,13 @@ pub fn main() !void {
     const a = [_]i32{ 1, 2, 3, 4, 5 };
     var end: usize = 4;
     const b = a[1..end];
+    const c = b[2..];
+    const d = a[1..4];
     std.debug.print("{}\n", .{@TypeOf(a)});
+    // runtime
     std.debug.print("{}\n", .{@TypeOf(b)});
+    // runtime
+    std.debug.print("{any}\n", .{@TypeOf(c)});
+    // comptime
+    std.debug.print("{any}\n", .{@TypeOf(d)});
 }
