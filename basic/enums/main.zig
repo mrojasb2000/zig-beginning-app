8e2b2488151992ee6d50df784a6917178a43bfac
mavro <mrojasb2000@gmail.com>
2023-10-13 18:52:04 -0300

    chore: add method on enums

--- basic/enums/main.zig
+++ basic/enums/main.zig
@@ -1,10 +1,25 @@
 const std = @import("std");
 
+const Status = enum {
+    ok,
+    bad,
+    unknown,
+};
+
+const Stage = enum {
+    validate,
+    awaiting_confirmation,
+    confirmed,
+    completed,
+    err,
+
+    pub fn isCompleted(self: Stage) bool {
+        return self == .confirmed or self == .err;
+    }
+};
+
 pub fn main() !void {
-    const Status = enum {
-        ok,
-        bad,
-        unknown,
-    };
     std.debug.print("{any}\n", .{Status.ok});
+    std.debug.print("{any}\n", .{Stage.completed});
+    std.debug.print("{any}\n", .{Stage.isCompleted(Stage.completed)});
 }
