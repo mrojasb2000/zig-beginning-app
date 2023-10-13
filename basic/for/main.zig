10af4a18e59390be0f9d0c319da699e8900711ec
mavro <mrojasb2000@gmail.com>
2023-10-13 11:01:22 -0300

    chore: add compare arrays generic function

--- basic/for/main.zig
+++ basic/for/main.zig
@@ -3,9 +3,11 @@ const std = @import("std");
 pub fn main() !void {
     // array literal
     const data = [_]u32{ 1, 2, 3, 4 };
+    const more_data = [_]u32{ 1, 2, 3, 4 };
 
     std.debug.print("Array contain 5 => {any}\n", .{contains(&data, 5)});
     std.debug.print("Array contain 3 => {any}\n", .{contains(&data, 3)});
+    std.debug.print("Array1 equals Array2 => {any}\n", .{eql(u32, &data, &more_data)});
 }
 
 // contains: check an array contains a value
@@ -16,4 +18,14 @@ fn contains(haystack: []const u32, neddle: u32) bool {
         }
     }
     return false;
+}
+
+pub fn eql(comptime T: type, a: []const T, b: []const T) bool {
+    // if they aren't the same length, the cant't be equal
+    if (a.len != b.len) return false;
+
+    for (a, b) |a_elem, b_elem| {
+        if (a_elem != b_elem) return false;
+    }
+    return true;
 }
