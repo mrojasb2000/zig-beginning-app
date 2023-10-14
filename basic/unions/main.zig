616c28f5de89c8d78ef65e9b84c37a3754a5835d
mavro <mrojasb2000@gmail.com>
2023-10-13 22:59:11 -0300

    chore: add tagged unions data types

--- basic/unions/main.zig
+++ basic/unions/main.zig
@@ -3,12 +3,15 @@ const std = @import("std");
 // An union defines a set of types that a values can have.
 
 pub fn main() !void {
-    const n = Number{ .int = 32 };
-    std.debug.print("{d}\n", .{n.int});
-    std.debug.print("{}\n", .{@TypeOf(n.int)});
+    const i = Number{ .int = 32 };
+    std.debug.print("{d}\n", .{i.int});
+    std.debug.print("{}\n", .{@TypeOf(i.int)});
     const f = Number{ .float = 3.2 };
     std.debug.print("{}\n", .{f.float});
     std.debug.print("{}\n", .{@TypeOf(f.float)});
+    const n = Number{ .nan = {} };
+    std.debug.print("{}\n", .{n.nan});
+    std.debug.print("{}\n", .{@TypeOf(n.nan)});
 }
 
 const Number = union {
