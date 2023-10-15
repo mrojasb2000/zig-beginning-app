f8906b057f1352d21b97914c48338aafabaddd58
mavro <mrojasb2000@gmail.com>
2023-10-15 13:27:22 -0300

    fix: shadowing variables names

--- basic/shadowing/main.zig
+++ basic/shadowing/main.zig
@@ -4,9 +4,9 @@ pub fn main() !void {}
 
 fn read(stream: std.net.Stream) ![]const u8 {
     var buf: [512]u8 = undefined;
-    const read = try stream.read(&buf);
-    if (read == 0) {
+    const read_stream = try stream.read(&buf);
+    if (read_stream == 0) {
         return error.Closed;
     }
-    return buf[0..read];
+    return buf[0..read_stream];
 }
