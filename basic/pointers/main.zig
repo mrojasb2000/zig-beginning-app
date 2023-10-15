b887bd437bb44ddf6c4b689503982abd5634a126
mavro <mrojasb2000@gmail.com>
2023-10-15 17:42:15 -0300

    chore: add change value from function with pointers

---
+++ basic/pointers/main.zig
@@ -0,0 +1,26 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var user = User{
+        .id = 1,
+        .power = 100,
+    };
+
+    // this line has been added
+    levelUp(&user);
+    std.debug.print("User {d} has power of {d}\n", .{ user.id, user.power });
+    std.debug.print("{*}\n{*}\n{*}\n", .{ &user, &user.id, &user.power });
+
+    const user_p = &user;
+    std.debug.print("{any}\n", .{@TypeOf(user_p)});
+}
+
+fn levelUp(user: *User) void {
+    var u = user;
+    u.power += 1;
+}
+
+pub const User = struct {
+    id: u64,
+    power: i32,
+};
