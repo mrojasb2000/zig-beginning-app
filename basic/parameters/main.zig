924f79347fbe178da8131eaf8cbcfb4d5850018e
mavro <mrojasb2000@gmail.com>
2023-10-15 19:42:18 -0300

    chore: add parameters on methods

---
+++ basic/parameters/main.zig
@@ -0,0 +1,21 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var user = User{
+        .id = 1,
+        .power = 100,
+    };
+    std.debug.print("Power level {d} before callback\n", .{user.power});
+    user.levelUp();
+    std.debug.print("Power level {d} after callback\n", .{user.power});
+}
+
+const User = struct {
+    id: u64,
+    power: i32,
+
+    fn levelUp(user: *User) void {
+        std.debug.print("{*}\n{*}\n", .{ &user, user });
+        user.power += 1;
+    }
+};
