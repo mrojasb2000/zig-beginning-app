194dd5ac4515ae783b5f717220d529484dacb489
mavro <mrojasb2000@gmail.com>
2023-10-14 22:26:14 -0300

    chore: add optional values

--- basic/optional/main.zig
+++ basic/optional/main.zig
@@ -3,6 +3,10 @@ const std = @import("std");
 pub fn main() !void {
     var home: ?[]const u8 = null;
     var name: ?[]const u8 = "John Doe";
-    std.debug.print("{any}\n", .{home});
-    std.debug.print("{any}\n", .{name});
+    if (home) |h| {
+        std.debug.print("{any}\n", .{h});
+    }
+    if (name) |n| {
+        std.debug.print("{any}\n", .{n});
+    }
 }
