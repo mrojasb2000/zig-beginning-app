a5d1713fd35f697b88ed376276d15f9d3aba6450
mavro <mrojasb2000@gmail.com>
2023-10-16 22:39:03 -0300

    chore: add methods

--- basic/nested/main.zig
+++ basic/nested/main.zig
@@ -3,7 +3,12 @@ const std = @import("std");
 pub fn main() !void {
     var name = [4]u8{ 'G', 'o', 'k', 'u' };
     const goku = User{ .id = 1, .power = 100, .name = name[0..] };
+    levelUp(goku);
     std.debug.print("Id: {d}\nName: {s}\nPower: {d}\n", .{ goku.id, goku.name, goku.power });
+}
+
+fn levelUp(user: User) void {
+    user.name[2] = '!';
 }
 
 pub const User = struct {
