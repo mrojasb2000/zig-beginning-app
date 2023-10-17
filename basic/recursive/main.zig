0e4ae879af66aceee164628599a8494a41a0c7c4
mavro <mrojasb2000@gmail.com>
2023-10-17 17:23:35 -0300

    chore: add recursive struct

---
+++ basic/recursive/main.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+
+pub fn main() !void {
+    const leto = User{
+        .id = 1,
+        .power = 9001,
+        .manager = null,
+    };
+
+    const duncan = User{
+        .id = 2,
+        .power = 9001,
+        .manager = &leto,
+    };
+
+    std.debug.print("{any}\n{any}", .{ leto, duncan });
+}
+
+pub const User = struct {
+    id: u64,
+    power: i32,
+    manager: ?*const User,
+};
